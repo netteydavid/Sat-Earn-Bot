a7bbcae8d45109a301753471172a8ed98f7a8824
netteydavid <netteydavid@live.com>
2021-11-28 22:09:44 -0600

    Reply with lightning invoice

--- comment bot.py
+++ comment bot.py
@@ -1,8 +1,12 @@
 from configparser import ConfigParser
 import praw
 import re
+import lnpay_py
+from lnpay_py.wallet import LNPayWallet
+import math
 
 PAY_CMD = r"!pay (\d+)"
+FEE_MULTIPLIER = 1.005
 
 def main():
     # Get user agent, client id, secret, username, and password from ini file
@@ -12,6 +16,7 @@ def main():
     userinfo = config_obj["USERINFO"]
     apiinfo = config_obj["APIINFO"]
     redditinfo = config_obj["REDDITINFO"]
+    lnpayinfo = config_obj["LNPAYINFO"]
 
     # Get Reddit
     reddit = praw.Reddit(
@@ -24,13 +29,16 @@ def main():
 
     # Compile regular expression
     pay_re = re.compile(PAY_CMD)
+
+    # Initilize LNPay
+    lnpay_py.initialize(lnpayinfo["public_key"])
     
     # Continuously cycle through comments
     subreddits = reddit.subreddit(redditinfo["subreddits"])
     for comment in subreddits.stream.comments(skip_existing=True):
-        command(comment, pay_re)
+        command(comment, pay_re, lnpayinfo["wallet_invoice"])
 
-def command(comment, pay_re):
+def command(comment, pay_re, invoice_key):
     # Get comment text
     text = comment.body.lower()
     
@@ -39,9 +47,25 @@ def command(comment, pay_re):
 
     # Check if there is a match
     if pay_match is not None:
-        amount = pay_match.group(1)
-        destination = comment.submission.author.name
-        comment.reply(f'INVOICE FOR {amount} to {destination}')
+        # Get amount
+        amount = int(pay_match.group(1))
+        # Add fee
+        amount = math.ceil(amount * FEE_MULTIPLIER)
+
+        # Get payer and payee
+        payer = comment.author.name
+        payee = comment.submission.author.name
+
+        # Get the wallet
+        se_wallet = LNPayWallet(invoice_key)
+
+        # Get invoice
+        invoice_params = { 'num_satoshis': amount, 'memo': f'{payer} to {payee}', 'expiry': 1200 }
+        invoice = se_wallet.create_invoice(invoice_params)
+
+        # Reply
+        # TODO: Host my own QR code generator
+        comment.reply(f'Lightning Invoice: >!{invoice["payment_request"]}!< \n\n[Pay Invoice via QR Code](https://api.qrserver.com/v1/create-qr-code/?data={invoice["payment_request"]})')
 
 if __name__ == "__main__":
     main()
